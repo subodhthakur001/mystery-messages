0b312b99c0bbadf00f943aa83cb55b0255046d92
subodh <skthakur.styles619@gmail.com>
2025-03-27 21:11:08 +0530

    next-auth setup

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,4 +1,57 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
 import UserModel from "@/model/User";
-import 
+import bcrypt from "bcrypt"
+import dbConnect  from "@/lib/dbConnect";
+
+export const nextAuthOptions : NextAuthOptions  = {
+    providers: [
+        CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            username: { label: "Email", type: "text", placeholder: "jsmith" },
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials: any) : Promise<any> {
+            await dbConnect()
+            try{
+               const dbUser =  await UserModel.findOne({
+                    $or: [
+                        {email: credentials.identifier},
+                        {username: credentials.identifier}
+                    ]
+                })
+                if(!dbUser){
+                    throw new Error("User does not exists")
+                }
+                if(!dbUser.isVerified){
+                    throw new Error("User is not verified, please verify the user")
+                }
+                const isPasswordCorrect = await bcrypt.compare(credentials.password, dbUser.password)
+                if(isPasswordCorrect){
+                   return dbUser 
+                }
+                else{
+                    throw new Error("Incorrect password")
+                }
+
+
+            }catch(error){
+                throw new Error(error)
+            }
+      
+            // If no error and we have user data, return it
+           
+            // Return null if user data could not be retrieved
+            return null
+          }
+        })
+      ],
+    pages :{
+        signIn: '/signin',
+    },
+    session : {
+        strategy : "jwt"
+    },
+    secret : process.env.NEXTAUTH_SECRET,
+}
